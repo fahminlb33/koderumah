5f440ed9d7615af51f36bad28d634143356eaa0f
rasyidf <ufarasfa@gmail.com>
2024-04-08 23:50:01 +0700

    Update Layout for Chatbot

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,8 +1,8 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
-import { LoaderFunctionArgs } from "@remix-run/cloudflare";
-import { Link, Outlet, json } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
 import { Button } from "~/components/ui/button";
 import { PageActions, PageHeader, PageHeaderDescription, PageHeaderHeading } from "~/components/ui/page-header";
+import { TitleBar } from "~/components/ui/shell-header";
 
 export const meta: MetaFunction = () => {
   return [
@@ -16,32 +16,35 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="container relative">
+    <div >
+      <TitleBar title="KodeRumah" />
+      <div className="container relative">
 
-      <PageHeader className="max-w-3xl h-full">
-        <PageHeaderHeading className="text-balance">
-          Welcome to KodeRumah
-        </PageHeaderHeading>
-        <PageHeaderDescription>
-          Analyse and learn about properties around you
-        </PageHeaderDescription>
-        <PageActions>
-          <Button asChild>
-            <Link to="app" unstable_viewTransition>Start</Link>
-          </Button>
-          <Button asChild variant="outline">
-            <a
-              href="https://github.com/fahminlb33/koderumah"
-              target="_blank"
-            >
-              GitHub
-            </a>
-          </Button>
-        </PageActions>
-      </PageHeader>
-      <section id="blocks" className="grid scroll-mt-24 gap-24 lg:gap-48">
-        <Outlet />
-      </section>
+        <PageHeader className="max-w-3xl h-full">
+          <PageHeaderHeading className="text-balance">
+            Welcome to KodeRumah
+          </PageHeaderHeading>
+          <PageHeaderDescription>
+            Analise and learn about properties around you
+          </PageHeaderDescription>
+          <PageActions>
+            <Button asChild>
+              <Link to="app" unstable_viewTransition>Start</Link>
+            </Button>
+            <Button asChild variant="outline">
+              <a
+                href="https://github.com/fahminlb33/koderumah"
+                target="_blank"
+              >
+                GitHub
+              </a>
+            </Button>
+          </PageActions>
+        </PageHeader>
+        <section id="blocks" className="grid scroll-mt-24 gap-24 lg:gap-48">
+          <Outlet />
+        </section>
+      </div>
     </div>
   );
 }
